c11828759a45247490c800824967eef3810eaf18
Wellington <welin0371734d@gmail.com>
2025-07-27 15:36:14 -0500

    controlador

--- src/controllers/MazeController.java
+++ src/controllers/MazeController.java
@@ -1,25 +1,42 @@
 package controllers;
 
 import models.*;
+import views.MazeFrame;
+import views.MazePanel;
+import views.ResultadosDialog;
 
 import java.util.List;
 
 public class MazeController {
     private Cell[][] mazeGrid; //va  a almacenar posiciones del laberinto
-    private SolveResults solveResults; //va a contener los resultados de los algoritmos
+    private final SolveResults solveResults; //va a contener los resultados de los algoritmos
 
-    public MazeController() {
+    private final MazeFrame mazeFrame;
+    private final MazePanel mazePanel;
+    private final ResultadosDialog resultadosDialog;
+
+    public MazeController(MazeFrame mazeFrame, MazePanel mazePanel, ResultadosDialog resultadosDialog) {
+        this.mazeFrame = mazeFrame;
+        this.mazePanel = mazePanel;
+        this.resultadosDialog = resultadosDialog;
         this.solveResults = new SolveResults();
     }
 
     public void setMaze(Cell[][] mazeGrid) {//va a leer las celdas del laberinto
         this.mazeGrid = mazeGrid;
+        mazePanel.setMazeGrid(mazeGrid);
     }
     
     //ejecuta como se va a resolver los laberintos y guarda el respuesta
     public void addAlgoritmoResult(String algorithmName, List<Cell> path, long executionTime) {
         AlgorithmResult result = new AlgorithmResult(algorithmName, executionTime, path);
         solveResults.addResult(result);
+        mazePanel.showSolutionPath(path);
+    }
+
+    public void mostrarResultados() {
+        resultadosDialog.setResults(solveResults.getResults()); //implementar en ResultadosDialog
+        resultadosDialog.setVisible(true);
     }
 
     public List<AlgorithmResult> getResults() {//devuelve los resultados que estaban almacenados
@@ -28,6 +45,7 @@ public class MazeController {
 
     public void clearResults() { //borra los resultados que staban almacenados 
         solveResults.clearResults();
+        mazePanel.clearSolution();
     }
 
     public Cell[][] getMaze() {//retorna el laberinto
